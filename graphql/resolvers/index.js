c1c84d5c4b8b99c245763d258bf92b61c377297f
Anant <anant.awasthy@tatadigital.com>
2020-11-17 19:34:29 +0530

    done

---
+++ graphql/resolvers/index.js
@@ -0,0 +1,15 @@
+const postResolvers = require("./posts");
+const userResolvers = require("./users");
+const commentResolvers = require("./comments");
+
+module.exports = {
+  Query: {
+    ...postResolvers.Query,
+    ...userResolvers.Query,
+  },
+  Mutation: {
+    ...postResolvers.Mutation,
+    ...userResolvers.Mutation,
+    ...commentResolvers.Mutation,
+  },
+};
